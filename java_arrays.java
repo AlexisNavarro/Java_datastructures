6ec80fa95ebf71285b466ad9fa06d993018a39c0
Alexis Navarro <navarroalexis007@gmail.com>
2025-04-21 17:01:32 -0600

    finished isAnagram function

--- java_arrays.java
+++ java_arrays.java
@@ -1,3 +1,4 @@
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -18,6 +19,29 @@ public class java_arrays {
         }
         return false;
     }// end has duplicate method
+
+    public boolean isAnagram(String s, String t) {
+        //check if the strings are the same length
+        if(s.length() != t.length()){
+            return false;
+        }
+        
+        //create two hash maps to count the letters for instance "a": 7
+        HashMap<Character, Integer> letter_count_s = new HashMap<>();
+        HashMap<Character, Integer> letter_count_t = new HashMap<>();
+
+
+        for(int i = 0 ; i < s.length(); i++){
+            //check if the current letter is in the map, if yes then increase the count, if not then add it to the map 
+            letter_count_s.put(s.charAt(i), letter_count_s.getOrDefault(s.charAt(i),0)+1);
+            letter_count_t.put(t.charAt(i), letter_count_t.getOrDefault(t.charAt(i),0)+1) ;
+
+        }
+        
+        //compare both maps such as having same values and counts
+        return letter_count_s.equals(letter_count_t);
+
+    }
 
     public static void main(String[] args) {
         java_arrays prob1 = new java_arrays();
